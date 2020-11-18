eaf97daceaf97af74f38e514a18dc164d637faf3
Brooke Miller <brookecm28@gmail.com>
2020-11-17 22:15:55 -0700

    Palindrome Complete

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -10,13 +10,40 @@ class Palindrome extends Component {
         }
     }
     
+    handleInputChange (e) {
+        this.setState ({
+            userInput: e
+        })
+    }
+
+    checkPalindrome (string) {
+      let newArr = []
+      for (let i = string.length - 1; i > -1; i--) {
+        newArr.push(string[i])
+      }
+    //   console.log(newArr)
+      let newStr = newArr.join('')
+    //   console.log(newStr)
+      if (newStr === string) {
+        //   console.log('yep')
+          this.setState ({
+              palindrome: 'true'
+          })
+      } else {
+        //   console.log('nope')
+          this.setState ({
+              palindrome: 'false'
+          })
+      }
+    }
+
     render () {
         return (
             <div className='puzzleBox filterStringPB'>
                 <h4>Palindrdome</h4>
-                <input className='inputLine' />
-                <button className='confirmationButton'>Check</button>
-                <span className='resultsBox'></span>
+                <input className='inputLine' onChange={(e) => this.handleInputChange(e.target.value)}/>
+                <button className='confirmationButton' onClick={() => {this.checkPalindrome(this.state.userInput)}}>Check</button>
+                <span className='resultsBox'>Palindrome: {this.state.palindrome}</span>
             </div>
         )
     }
